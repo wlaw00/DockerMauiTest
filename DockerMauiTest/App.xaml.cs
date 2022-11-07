2b20399596b21c762cb540b3206b1996e99d82e8
Wai-Kei Law <57984020+wlaw00@users.noreply.github.com>
2022-11-07 16:04:53 +0100

    Init maui project

---
+++ DockerMauiTest/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DockerMauiTest;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DockerMauiTest/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DockerMauiTest;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
